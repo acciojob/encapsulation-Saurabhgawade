402ac95234de2fa6496de781d6e8513bf13013af
Saurabhgawade <gawadesaurabh26@gmail.com>
2023-09-13 14:54:00 +0530

    firsttime

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+
+    RWOnly only=new RWOnly();
+    only.setName("saurabh");
+    System.out.println(only.getName());
+  }
 }
